73ac287844fd63bee4b11e223815444c6c78168e
Denis <denisrubel40@gmail.com>
2023-09-21 23:57:44 +0300

    Added navbar menu with login option

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import {BrowserModule} from '@angular/platform-browser'
 import {AppComponent} from './app.component'
 import {AppRoutingModule} from './app-routing.module'
 import {HttpClientModule} from '@angular/common/http'
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NavComponent} from './nav/nav.component'
+import {FormsModule} from '@angular/forms'
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [AppComponent, NavComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
