75d74420284ddb61c2a1ee5dfb77898d5761fbda
Denis <denisrubel40@gmail.com>
2023-10-10 23:17:19 +0300

    Reworked the messaging feature using SignalR

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,68 +1,91 @@
-import {Component, OnInit, ViewChild} from '@angular/core'
-import {Member} from "../../_models/member";
-import {MembersService} from "../../_services/members.service";
-import {ActivatedRoute} from "@angular/router";
-import {CommonModule} from "@angular/common";
-import {TabDirective, TabsetComponent, TabsModule} from "ngx-bootstrap/tabs";
-import {GalleryItem, GalleryModule, ImageItem} from "ng-gallery";
-import {TimeagoModule} from "ngx-timeago";
-import {MemberMessagesComponent} from "../member-messages/member-messages.component";
-import {MessageService} from "../../_services/message.service";
-import {Message} from "../../_models/message";
-import {PresenceService} from "../../_services/presence.service";
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core'
+import {Member} from '../../_models/member'
+import {MembersService} from '../../_services/members.service'
+import {ActivatedRoute} from '@angular/router'
+import {CommonModule} from '@angular/common'
+import {TabDirective, TabsetComponent, TabsModule} from 'ngx-bootstrap/tabs'
+import {GalleryItem, GalleryModule, ImageItem} from 'ng-gallery'
+import {TimeagoModule} from 'ngx-timeago'
+import {MemberMessagesComponent} from '../member-messages/member-messages.component'
+import {MessageService} from '../../_services/message.service'
+import {Message} from '../../_models/message'
+import {PresenceService} from '../../_services/presence.service'
+import {AccountService} from '../../_services/account.service'
+import {User} from '../../_models/user'
+import {take} from 'rxjs'
 
 @Component({
   selector: 'socialSync-member-detail',
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
   standalone: true,
-  imports: [CommonModule, TabsModule, GalleryModule, TimeagoModule, MemberMessagesComponent]
+  imports: [
+    CommonModule,
+    TabsModule,
+    GalleryModule,
+    TimeagoModule,
+    MemberMessagesComponent,
+  ],
 })
-export class MemberDetailComponent implements OnInit {
+export class MemberDetailComponent implements OnInit, OnDestroy {
   @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent
   member: Member = {} as Member
   images: GalleryItem[] = []
   activeTab?: TabDirective
   messages: Message[] = []
+  user?: User
 
   constructor(
     private memberService: MembersService,
+    private accountService: AccountService,
     private route: ActivatedRoute,
     private messageService: MessageService,
-    public presenceService: PresenceService
-  ) {}
+    public presenceService: PresenceService,
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) this.user = user
+      },
+    })
+  }
 
   ngOnInit(): void {
     this.route.data.subscribe({
-      next: data => this.member = data['member']
+      next: (data) => (this.member = data['member']),
     })
 
     this.route.queryParams.subscribe({
-      next: params => {
+      next: (params) => {
         params['tab'] && this.selectTab(params['tab'])
-      }
+      },
     })
 
     this.getImages()
   }
 
-  onTabActivated(data: TabDirective){
+  ngOnDestroy(): void {
+    this.messageService.stopHubConnection()
+  }
+
+  onTabActivated(data: TabDirective) {
     this.activeTab = data
-    if(this.activeTab.heading === 'Messages'){
-      this.loadMessages()
+    if (this.activeTab.heading === 'Messages' && this.user) {
+      this.messageService.createHubConnection(this.user, this.member.userName)
+    } else {
+      this.messageService.stopHubConnection()
     }
   }
 
   selectTab(heading: string) {
     if (this.memberTabs) {
-      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+      this.memberTabs.tabs.find((x) => x.heading === heading)!.active = true
     }
   }
 
-  loadMessages(){
-    if(this.member){
+  loadMessages() {
+    if (this.member) {
       this.messageService.getMessageThread(this.member.userName).subscribe({
-        next: messages => this.messages = messages
+        next: (messages) => (this.messages = messages),
       })
     }
   }
--- src/app/members/member-messages/member-messages.component.ts
+++ src/app/members/member-messages/member-messages.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input, ViewChild} from '@angular/core';
-import {Message} from "../../_models/message";
 import {FormsModule, NgForm} from "@angular/forms";
 import {MessageService} from "../../_services/message.service";
 import {CommonModule} from "@angular/common";
@@ -15,19 +14,15 @@ import {TimeagoModule} from "ngx-timeago";
 export class MemberMessagesComponent{
   @ViewChild('messageForm') messageForm?: NgForm;
   @Input() username?: string;
-  @Input() messages: Message[] = [];
   messageContent = '';
 
-  constructor(private messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
   sendMessage() {
-    if (!this.username) return;
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
-      next: message => {
-        this.messages.push(message);
-        this.messageForm?.reset();
-      }
+    if (!this.username)
+      return;
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+      this.messageForm?.reset();
     })
   }
-
 }
